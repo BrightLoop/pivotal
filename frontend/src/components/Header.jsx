bed61d943a6793a0fda28a89cd7402cf84cdc31d
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2024-12-10 19:37:55 +0530

    Header changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
+import "./Components.css";
 const Header = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
@@ -8,7 +9,7 @@ const Header = () => {
     document.body.classList.toggle("dark-mode", !isDarkMode);
   };
   return (
-    <div className="bg-slate-700 flex justify-between items-center  p-7 sticky top-10 rounded-2xl">
+    <div className=" flex justify-between items-center  p-7 sticky top-10 rounded-2xl header-home">
       <div className="header">
         <ul className="flex gap-10 text-white">
           <li>Home</li>
