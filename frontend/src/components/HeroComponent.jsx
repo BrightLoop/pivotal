ccb04854f3f3ef95dca58d0a96a9ceed5793c111
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2024-12-10 19:39:06 +0530

    small change

--- frontend/src/components/HeroComponent.jsx
+++ frontend/src/components/HeroComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroComponent = () => {
   return (
     <div className="relative flex justify-center w-full h-screen">
-      <h1 className="text-black">Boost Your Productivity with Pivotal</h1>
+      <h1 className="text-black">Boost Your Productivity with </h1>
     </div>
   );
 };
