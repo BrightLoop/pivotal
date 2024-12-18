ec9a17a2e1bf5084c125f3b327ce93ab31c50853
Ashif Hassan <ashifhassan1996@gmail.com>
2024-12-18 13:28:16 +0530

    Add redirect url to the json data

--- backend/src/controllers/indexControllers.js
+++ backend/src/controllers/indexControllers.js
@@ -2,8 +2,9 @@ import httpStatusCodes from "../utils/httpStatusCodes.js";
 
 const getHome = (req, res) => {
     const locals = { title: "Home | Pivotal" };
-    return res.status(httpStatusCodes.OK).render("home", {
-        locals,
+    return res.status(httpStatusCodes.OK).json({
+        message: "Welcome to Pivotal Home.",
+        redirectUrl: "/",
     });
 };
 
